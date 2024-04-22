6f6a6f296108860c2d89273958f85201976f35f0
DAG WEG <dagtef2@gmail.com>
2024-04-22 20:54:01 +0300

    feat: y-offset-normalization

--- Assets/cubes.cs
+++ Assets/cubes.cs
@@ -8,7 +8,7 @@ using Random = UnityEngine.Random;
 public class cubes : MonoBehaviour
 {
 
-    List<GameObject> list = new();
+    List<GameObject> cubz = new();
 
     [SerializeField] GameObject cube;
     [SerializeField] Vector3 spacing;
@@ -19,27 +19,39 @@ public class cubes : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     
+        PopulateCubes();
+    }
+
+    void PopulateCubes(){
         Vector3 curCubePos = cube.transform.position;
+
+        Bounds cubeBounds = CalculateBounds(cube);
 
         for(int i = 0; i < 10; i ++){
             
+            // Instantiate a new cube
             GameObject cubeInst = Instantiate(cube,curCubePos,Quaternion.identity);
-            Bounds bounds = CalculateBounds(cubeInst);
-            float yExtent = bounds.extents.y;
-            
-            if(CompareSize(cubeInst,cube) == -1){
-                cubeInst.transform.position += transform.TransformPoint(0, yExtent,0);
-            }
-            else{
-                cubeInst.transform.position -= transform.TransformPoint(0, yExtent,0);
-            }
 
-            curCubePos += spacing;
-
+            // Apply random y scaling
             float rand = Random.Range(1,heightDifference);
             Vector3 scale = cubeInst.transform.localScale;
             cubeInst.transform.localScale = new Vector3(scale.x,scale.y * rand,scale.z);
-            list.Add(cubeInst);     
+            
+            // Normalize the distance from origin
+            Bounds bounds = CalculateBounds(cubeInst);
+            Debug.Log(bounds.extents.y + " and " + cubeBounds.extents.y);
+            float deltaFromOrigin = Mathf.Abs(bounds.extents.y - cubeBounds.extents.y);
+
+            if(CompareSize(cubeInst,cube) == -1){
+                cubeInst.transform.position += transform.TransformPoint(0, deltaFromOrigin,0);
+            }
+            else{
+                cubeInst.transform.position -= transform.TransformPoint(0, deltaFromOrigin,0);
+            }
+
+            // Change cubePosition by spacing for the next iteration
+            curCubePos += spacing;            
+            cubz.Add(cubeInst);     
             
         }
     }
